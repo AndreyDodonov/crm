d0238e3b481517043717f74c566f0fb356972a1e
AndreyDodonov <Nordhummer@gmail.com>
2019-10-16 22:21:24 +0300

    + mongoDB connect

--- app.js
+++ app.js
@@ -2,12 +2,22 @@
 
 const express = require('express'),
       app = express(),
+      mongoose = require('mongoose'),
       bodyParser = require('body-parser'),
+      keys = require('./config/keys'),
       authRouts = require('./routes/auth'),
       analyticRouts = require('./routes/analytic'),
       categoryRouts = require('./routes/category'),
       orderRouts = require('./routes/order'),
       positionRouts = require('./routes/position');
+
+mongoose.connect(keys.mongoURI)
+    .then ( () => {
+        console.log('MongoDB connected');
+    })
+    .catch ((error) => {
+        console.log(error);
+    });
 
 app.use(require('morgan')('dev'));
 app.use(require('cors')());
