4943cb9b8879889058d11cde05285016b501280b
AndreyDodonov <Nordhummer@gmail.com>
2019-10-16 21:42:41 +0300

    + models

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 ![learn project](https://img.shields.io/badge/Learn-learn%20project-orange.svg)
 
-### Test MEAN steck
+### Test MEAN stack
+
+- Node.js
+- Express
+- Mongoose
+- MongoDB
 
 
--- app.js
+++ app.js
@@ -2,12 +2,17 @@
 
 const express = require('express'),
       app = express(),
+      bodyParser = require('body-parser'),
       authRouts = require('./routes/auth'),
       analyticRouts = require('./routes/analytic'),
       categoryRouts = require('./routes/category'),
       orderRouts = require('./routes/order'),
       positionRouts = require('./routes/position');
 
+app.use(require('morgan')('dev'));
+app.use(require('cors')());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 app.use('/api/auth', authRouts);
 app.use('/api/analytic', analyticRouts);
 app.use('/api/category', categoryRouts);
