b155c2eb7b02e51f9df02f40b324f3c161cd089f
AndreyDodonov <Nordhummer@gmail.com>
2019-10-22 00:00:05 +0300

    + encryption to password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,31 @@
 'use strict';
-
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 module.exports.login = function(req, res) {
   
 };
 
-module.exports.register = function(req, res) {    
-    const user = new User({
-        email: req.body.email,
-        password: req.body.password
-    });
-    user.save().then (() => console.log('user created'));
+module.exports.register = async function(req, res) {    
+    const candidate = await User.findOne({email: req.body.email});
+    if (candidate) {
+        // if user exist - send error
+        res.status(409).json({
+            message: 'Такой email уже занят'
+        });
+    } else {
+        // create user
+        const salt = bcrypt.genSaltSync(10);
+        const password = req.body.password;
+        const user = new User ({
+            email : req.body.email,
+            password: bcrypt.hashSync(password, salt)
+        });
+        try {
+        await user.save();
+         res.status(201).json(user);
+        } catch(e) {
+            // TODO error code
+        }
+    }
 };
