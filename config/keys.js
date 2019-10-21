d6f252876e031635ed5939a013c325a61604ec97
AndreyDodonov <Nordhummer@gmail.com>
2019-10-22 00:36:19 +0300

    + login

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = {
-    mongoURI: 'mongodb+srv://ElKaban:Cain-666@clustercrm-cfcnr.gcp.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://ElKaban:Cain-666@clustercrm-cfcnr.gcp.mongodb.net/test?retryWrites=true&w=majority',
+    jwt: 'dev-jwt'
 };
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,37 @@
 'use strict';
-const bcrypt = require('bcryptjs');
-const User = require('../models/User');
+const bcrypt = require('bcryptjs'),
+      User = require('../models/User'),
+      jwt = require('jsonwebtoken'),
+      keys = require('../config/keys');
 
-module.exports.login = function(req, res) {
-  
+module.exports.login = async function(req, res) {
+    const candidate = await User.findOne({
+        email: req.body.email
+    });
+    if (candidate) {
+        // user found, check password
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password) ;
+        if (passwordResult) {
+            // token generation
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id
+            }, keys.jwt, {expiresIn: 3600}) ;
+            res.status(200).json({
+                token: `Bearer ${token}`
+            })
+        } else {
+            //error. Password incorrect
+            res.status(401).json({
+                message:'Пароли не совпадают'
+            });
+        }
+    } else {
+        // user not found, error
+        res.status(404).json({
+            message: 'Пользователь не найден'
+        });
+    }
 };
 
 module.exports.register = async function(req, res) {    
