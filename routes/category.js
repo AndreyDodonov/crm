667a26b23f1cab3c1bdbfd8c52cc05c955bd6622
AndreyDodonov <Nordhummer@gmail.com>
2019-10-27 18:22:25 +0300

    + methods for categories

--- routes/category.js
+++ routes/category.js
@@ -3,13 +3,14 @@
 const express = require('express'),
       router = express.Router(),
       passport = require('passport'),
-      controller = require('../controllers/category');
+      controller = require('../controllers/category'),
+      upload = require('../middleware/upload');
 
 router.get('/', passport.authenticate('jwt', {session: false}), controller.getAll);
-router.get('/:id', controller.getById);
-router.delete('/:id', controller.remove);
-router.post('/:id', controller.create);
-router.patch('/:id', controller.update);
+router.get('/:id', passport.authenticate('jwt', {session: false}), controller.getById);
+router.delete('/:id', passport.authenticate('jwt', {session: false}), controller.remove);
+router.post('/', passport.authenticate('jwt', {session: false}), upload.single('image'), controller.create);
+router.patch('/:id', passport.authenticate('jwt', {session: false}), upload.single('image'), controller.update);
 
 
 
