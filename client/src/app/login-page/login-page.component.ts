cdfe977d80be110f6cfd040f4aaba2728bea4ef8
AndreyDodonov <Nordhummer@gmail.com>
2019-10-30 22:54:55 +0300

    login form is working (test in console )

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../shared/services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
 
   form: FormGroup;
+  aSub: Subscription
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
     this.form = new FormGroup({
@@ -18,8 +21,24 @@ export class LoginPageComponent implements OnInit {
       password: new FormControl(null, [Validators.required, Validators.minLength(6)])
     })
   }
-  onSubmit() {
-    
+
+  ngOnDestroy() {
+    if (this.aSub){
+    this.aSub.unsubscribe()
+    }
   }
 
+  onSubmit() {
+    this.form.disable();
+    const user = {
+      email:this.form.value.email,
+      password: this.form.value.password
+    }
+    this.aSub = this.auth.login(user).subscribe(
+      () => console.log('login success'),
+      error => {console.warn(error)
+      this.form.enable()
+      }
+    )
+  }
 }
--- client/src/app/shared/services/auth.service.ts
+++ client/src/app/shared/services/auth.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from 'selenium-webdriver/http';
+import { HttpClient } from '@angular/common/http';
 import {User} from '../interfaces';
+import { Observable } from 'rxjs';
 
 
-@Injectable
+@Injectable({
+    providedIn: 'root'
+})
 
 export class AuthService {
     constructor(private http: HttpClient) {}
 
     register() {}
 
-    login(user: User) {}
+    login(user: User): Observable<{token: string}> {
+      return  this.http.post<{token: string}>('/api/auth/login', user)
+    }
 
 }
